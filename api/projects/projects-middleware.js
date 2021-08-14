79e399b90831436b673a2cf1820b96828d9a6723
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-14 07:33:53 -0400

    findProjects middleware built, and began working on projects-router

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,34 @@
 // add middlewares here related to projects
+
+const {get} = require('./projects-model')
+
+const findProject = (req, res, next) => {
+
+    const {id} = req.params;
+
+    if (!id) {
+        get()
+        .then(resolve => {
+            req.projectsArray = resolve;
+            next();
+        })
+        .catch(err => {
+            res.status(500).json({message: "Unable to retrieve projects array"});
+        });
+    } else {
+        get(id)
+        .then(resolve => {
+            req.project = resolve;
+            next();
+        })
+        .catch(err => {
+            res.status(404).json({message: "project not found"})
+            next();
+        })
+    }
+
+}
+
+module.exports = {
+    findProject
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,60 @@
 // Write your "projects" router here!
+
+const {get, insert, update, remove, getProjectActions} = require('./projects-model')
+
+const {findProject} = require('./projects-middleware')
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/", findProject, (req, res) => {
+
+    const {projectsArray} = req;
+
+    if (projectsArray) {
+        res.status(200).json(projectsArray);
+    } else {
+        res.status(200).json([])
+    }
+
+})
+
+router.get("/:id", findProject, (req, res) => {
+
+    const {project} = req;
+
+    res.status(200).json(project)
+
+})
+
+router.post("/", (req, res) => {
+    
+    const {body} = req;
+
+    if (!body.name ||
+        !body.description ||
+        typeof body.name !== 'string' ||
+        typeof body.description !== 'string') {
+            res.status(400).json({message: "Post does not meet requirements"})
+        } else {
+            insert(body)
+            .then(resolve => {
+                res.status(201).json(resolve)
+            })
+            .catch(err => {
+                res.status(500).json({message: "New project could not be submitted at this time"})
+            })
+        }
+
+})
+
+router.put("/:id", findProject, (req, res) => {
+    
+})
+
+router.get('*', (req, res) => {
+    res.status(200).json({message: "projects router running"})
+})
+
+module.exports = router;
