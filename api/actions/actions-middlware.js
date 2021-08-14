7867669537e9fdccdac0d7fd4631f192937eed0e
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-14 16:11:13 -0400

    finished first two routes within actionsRouter, built findActions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to actions
+const {get} = require('./actions-model');
+
+const findActions = (req, res, next) => {
+
+    const {id} = req.params;
+
+    if (!id) {
+        get()
+        .then(resolve => {
+            req.actions = resolve;
+            next();
+        })
+        .catch(err => {
+            res.status(500).json({message: "Unable to retrieve actions array"});
+        })
+    } else {
+        get(id)
+        .then(resolve => {
+            req.actions = resolve;
+            if (resolve) {
+                next();
+            } else {
+                res.status(404).json({message: 'Not a valid actions id'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: "Unable to retrieve actions array"})
+        })
+    }
+
+}
+
+module.exports = {
+    findActions
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,7 +2,29 @@
 
 const express = require('express');
 
+const {findActions} = require('./actions-middlware');
+
 const router = express.Router();
+
+router.get('/', findActions, (req, res) => {
+
+    const {actions} = req;
+
+    if (actions) {
+        res.status(200).json(actions);
+    } else {
+        res.status(200).json([])
+    }
+
+})
+
+router.get('/:id', findActions, (req, res) => {
+
+    const {actions} = req;
+
+    res.status(200).json(actions)
+
+})
 
 router.get('*', (req, res) => {
     res.status(200).json({message: 'actions router working'})
